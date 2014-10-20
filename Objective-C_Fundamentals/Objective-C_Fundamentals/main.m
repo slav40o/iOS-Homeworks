be7e49063b274aacffb0dc205f07746b6c5feadc
Slavi <slavicvetanov@gmail.com>
2014-10-21 01:09:53 +0300

    Initial project for the course homeworks
    s

---
+++ Objective-C_Fundamentals/Objective-C_Fundamentals/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C_Fundamentals
+//
+//  Created by Slavi on 10/21/14.
+//  Copyright (c) 2014 Slavi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
